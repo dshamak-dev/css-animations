c20774ad35641986504c1967fcd6885b7d813929
DShamak <dmitry.shamak.dev@gmail.com>
2023-11-05 07:38:21 +0400

    Add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,67 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (env) => {
-  const rootFolder = 'public';
+  const rootFolder = "public";
 
-  console.log('Production: ', env);
+  console.log("Production: ", env);
 
   return {
-    entry: './src/index.ts',
+    entry: "./src/index.ts",
     output: {
-      chunkFilename: '[name].[contenthash].js',
-      filename: '[name].[contenthash].js',
-      assetModuleFilename: '[name].[contenthash][ext][query]',
+      chunkFilename: "[name].[contenthash].js",
+      filename: "[name].[contenthash].js",
+      assetModuleFilename: "[name].[contenthash][ext][query]",
       asyncChunks: true,
       path: path.resolve(__dirname, rootFolder),
       clean: true,
-      publicPath: '/',
+      publicPath: "/",
     },
-    devtool: 'source-map',
+    devtool: "source-map",
     devServer: {
       static: {
         directory: path.join(__dirname, rootFolder),
       },
       historyApiFallback: {
-        index: '/'
+        index: "/",
       },
       port: 8008,
     },
     plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+        ignoreOrder: false,
+      }),
       new HtmlWebpackPlugin({
-        template: path.join(__dirname, 'index.html'),
+        template: path.join(__dirname, "index.html"),
       }),
     ],
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader', 'postcss-loader'],
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "postcss-loader",
+          ],
         },
         {
           test: /\.([tj]s)$/,
           exclude: /node_modules/,
-          enforce: 'pre',
-          use: ['babel-loader', 'source-map-loader'],
+          enforce: "pre",
+          use: ["babel-loader", "source-map-loader"],
         },
       ],
     },
     resolve: {
       alias: {
-        src: '/src',
+        src: "/src",
       },
-      modules: [path.resolve('./node_modules'), path.resolve('./src')],
-      extensions: ['.*', '.js', '.ts'],
+      modules: [path.resolve("./node_modules"), path.resolve("./src")],
+      extensions: [".*", ".js", ".ts"],
     },
     stats: {
       children: true,
@@ -61,7 +71,7 @@ module.exports = (env) => {
       usedExports: false,
       minimize: true,
       splitChunks: {
-        chunks: 'async',
+        chunks: "async",
       },
     },
   };
