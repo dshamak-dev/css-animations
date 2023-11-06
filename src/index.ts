1896fe8704ecf4f5cd4a742cba114abee010e43a
DShamak <dmitry.shamak.dev@gmail.com>
2023-11-06 21:42:00 +0400

    Fix merge conflict

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-import './styles.css';
+import './style.css';
+
+import initGradient from './gradient/gradient.script';
+import initScrollText from './scroll-text/scroll-text.script';
+
+initGradient();
+initScrollText();
